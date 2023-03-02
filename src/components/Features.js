d36b3acaa22bd2dc93b80ca1430c2c45a6bffdf1
Fernando Cases <nandocasesgarcia@gmail.com>
2023-03-02 09:42:06 +0100

    Feat: add new feature content

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,12 +1,48 @@
-function Features() {
+import styled from 'styled-components';
+
+const FeaturesContainer = styled.div`
+	background-color: #f9f9f9;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	padding: 2rem;
+`;
+
+const FeaturesTitle = styled.h2`
+	font-size: 2.5rem;
+	margin-bottom: 1rem;
+	color: #222;
+`;
+
+const FeaturesList = styled.ul`
+	list-style: none;
+	margin: 2rem 0;
+	padding: 0;
+`;
+
+const FeatureItem = styled.li`
+	font-size: 1.5rem;
+	margin-bottom: 0.5rem;
+	color: #555;
+`;
+
+const Features = () => {
 	return (
-		<div>
-			<h1>Features</h1>
-			<p>
-				Here is where i would put the content of the site. Features Features
-			</p>
-		</div>
+		<FeaturesContainer>
+			<FeaturesTitle>Wowly Features</FeaturesTitle>
+			<FeaturesList>
+				<FeatureItem>Book a surprise trip for you and your partner</FeatureItem>
+				<FeatureItem>
+					Choose from various trip themes and activities
+				</FeatureItem>
+				<FeatureItem>Customize your trip preferences</FeatureItem>
+				<FeatureItem>Get a curated itinerary for your trip</FeatureItem>
+				<FeatureItem>Surprise your partner with a unique adventure</FeatureItem>
+			</FeaturesList>
+		</FeaturesContainer>
 	);
-}
+};
 
 export default Features;
